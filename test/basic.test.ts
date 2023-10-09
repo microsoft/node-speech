58561cfcfff5f573c986cc97ce47dbf3f0258579
João Moreno <joao.moreno@microsoft.com>
2023-10-09 20:42:30 +0200

    cleanup tests

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -3,32 +3,31 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// import transcribe, { TranscriptionStatusCode } from '../index';
-import { speechapi } from '../index';
+import transcribe, { speechapi, TranscriptionStatusCode } from '../index';
 
 describe('Basics', () => {
 
-	// function transcribeOnce() {
-	// 	const controller = new AbortController();
+	function transcribeOnce() {
+		const controller = new AbortController();
 
-	// 	return new Promise<void>(resolve => {
-	// 		transcribe((error, result) => {
-	// 			switch (result.status) {
-	// 				case TranscriptionStatusCode.STARTED:
-	// 					setTimeout(() => controller.abort(), 1);
-	// 					break;
-	// 				case TranscriptionStatusCode.STOPPED:
-	// 					resolve();
-	// 					break;
-	// 			}
-	// 		}, { signal: controller.signal });
-	// 	});
-	// }
+		return new Promise<void>(resolve => {
+			transcribe((error, result) => {
+				switch (result.status) {
+					case TranscriptionStatusCode.STARTED:
+						setTimeout(() => controller.abort(), 1);
+						break;
+					case TranscriptionStatusCode.STOPPED:
+						resolve();
+						break;
+				}
+			}, { signal: controller.signal });
+		});
+	}
 
-	// test('it should be able to start and stop', async () => {
-	// 	await transcribeOnce();
-	// 	await transcribeOnce();
-	// }, 60000);
+	test('it should be able to start and stop', async () => {
+		await transcribeOnce();
+		await transcribeOnce();
+	}, 60000);
 
 	test('it should load the native module', () => {
 		expect(speechapi).toEqual(expect.objectContaining({
