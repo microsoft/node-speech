328f905256cb3152ac2455fe1a8d60dbdc158e8f
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-10-10 21:19:17 +0200

    try to fix pipeline

--- preinstall.ts
+++ preinstall.ts
@@ -3,10 +3,10 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const fs = require('fs');
-const path = require('path');
-const got = require('got');
-const decompress = require('decompress');
+import * as fs from 'fs';
+import { join } from 'path';
+import got from 'got';
+import decompress from 'decompress';
 
 const packages = {
   'Microsoft.CognitiveServices.Speech': '1.32.1',
@@ -16,21 +16,21 @@ const packages = {
   // 'Microsoft.CognitiveServices.Speech.Extension.Telemetry': '1.32.1',
 };
 
-async function main(packages) {
-  const cacheDir = path.join(__dirname, '.cache');
-  await fs.promises.mkdir(path.join(cacheDir, 'packages'), { recursive: true });
-  await fs.promises.mkdir(path.join(cacheDir, 'SpeechSDK'), { recursive: true });
+async function main(packages: Record<string, string>): Promise<void> {
+  const cacheDir = join(__dirname, '.cache');
+  await fs.promises.mkdir(join(cacheDir, 'packages'), { recursive: true });
+  await fs.promises.mkdir(join(cacheDir, 'SpeechSDK'), { recursive: true });
 
   for (const [name, version] of Object.entries(packages)) {
     const url = `https://www.nuget.org/api/v2/package/${name}/${version}`;
-    const packagePath = path.join(cacheDir, 'packages', `${name}-${version}.nupkg`);
+    const packagePath = join(cacheDir, 'packages', `${name}-${version}.nupkg`);
 
     if (fs.existsSync(packagePath)) {
       console.log(`Found ${name} ${version} in cache, skipping.`);
       continue;
     }
 
-    const sdkPath = path.join(cacheDir, 'SpeechSDK');
+    const sdkPath = join(cacheDir, 'SpeechSDK');
 
     const downloadStream = got.stream(url);
     const fileWriterStream = fs.createWriteStream(packagePath);
