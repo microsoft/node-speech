ddc4c12176bed3a1b6e8da9a93d474565bd1af8b
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-10-18 17:11:48 +0200

    vsda simple

--- src/main.cc
+++ src/main.cc
@@ -11,6 +11,9 @@
 #include <string>
 #include <unordered_map>
 
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+
 using namespace Microsoft::CognitiveServices::Speech;
 using namespace Microsoft::CognitiveServices::Speech::Audio;
 
@@ -242,9 +245,184 @@ private:
   std::string model;
 };
 
+const int GCM_TAG_LEN = 16;
+const int GCM_IV_LEN = 12;
+
+bool encrypt(const std::string &plainText, const std::string &key,
+             std::string &cipherText, std::string &iv, std::string &tag)
+{
+  EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+  if (!ctx)
+  {
+    return false;
+  }
+
+  if (1 != EVP_EncryptInit(ctx, EVP_aes_256_gcm(),
+                           reinterpret_cast<const unsigned char *>(key.data()), nullptr))
+  {
+    EVP_CIPHER_CTX_free(ctx);
+    return false;
+  }
+
+  std::vector<unsigned char> iv_buf(GCM_IV_LEN);
+  RAND_bytes(iv_buf.data(), GCM_IV_LEN);
+  iv = std::string(reinterpret_cast<char *>(iv_buf.data()), GCM_IV_LEN);
+
+  if (1 != EVP_EncryptInit_ex(ctx, nullptr, nullptr, nullptr, iv_buf.data()))
+  {
+    EVP_CIPHER_CTX_free(ctx);
+    return false;
+  }
+
+  int len;
+  std::vector<unsigned char> cipher_buf(plainText.size());
+
+  if (1 != EVP_EncryptUpdate(ctx, cipher_buf.data(), &len,
+                             reinterpret_cast<const unsigned char *>(plainText.data()), plainText.size()))
+  {
+    EVP_CIPHER_CTX_free(ctx);
+    return false;
+  }
+
+  int cipher_len = len;
+
+  if (1 != EVP_EncryptFinal_ex(ctx, cipher_buf.data() + len, &len))
+  {
+    EVP_CIPHER_CTX_free(ctx);
+    return false;
+  }
+
+  cipher_len += len;
+  cipherText = std::string(reinterpret_cast<char *>(cipher_buf.data()), cipher_len);
+
+  std::vector<unsigned char> tag_buf(GCM_TAG_LEN);
+  if (1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, GCM_TAG_LEN, tag_buf.data()))
+  {
+    EVP_CIPHER_CTX_free(ctx);
+    return false;
+  }
+  tag = std::string(reinterpret_cast<char *>(tag_buf.data()), GCM_TAG_LEN);
+
+  EVP_CIPHER_CTX_free(ctx);
+  return true;
+}
+
+bool decrypt(const std::string &cipherText, const std::string &key, const std::string &iv,
+             const std::string &tag, std::string &plainText)
+{
+  EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+  if (!ctx)
+  {
+    return false;
+  }
+
+  if (!EVP_DecryptInit(ctx, EVP_aes_256_gcm(),
+                       reinterpret_cast<const unsigned char *>(key.data()), nullptr))
+  {
+    EVP_CIPHER_CTX_free(ctx);
+    return false;
+  }
+
+  if (!EVP_DecryptInit_ex(ctx, nullptr, nullptr, nullptr,
+                          reinterpret_cast<const unsigned char *>(iv.data())))
+  {
+    EVP_CIPHER_CTX_free(ctx);
+    return false;
+  }
+
+  if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, GCM_TAG_LEN,
+                           reinterpret_cast<void *>(const_cast<char *>(tag.data()))))
+  {
+    EVP_CIPHER_CTX_free(ctx);
+    return false;
+  }
+
+  int len;
+  std::vector<unsigned char> plain_buf(cipherText.size());
+
+  if (!EVP_DecryptUpdate(ctx, plain_buf.data(), &len,
+                         reinterpret_cast<const unsigned char *>(cipherText.data()), cipherText.size()))
+  {
+    EVP_CIPHER_CTX_free(ctx);
+    return false;
+  }
+
+  int plain_len = len;
+
+  if (EVP_DecryptFinal_ex(ctx, plain_buf.data() + len, &len) <= 0)
+  {
+    EVP_CIPHER_CTX_free(ctx);
+    return false;
+  }
+
+  plain_len += len;
+  plainText = std::string(reinterpret_cast<char *>(plain_buf.data()), plain_len);
+
+  EVP_CIPHER_CTX_free(ctx);
+  return true;
+}
+
+std::string derive_key(const std::string& input) {
+    const EVP_MD* md = EVP_sha256();
+    unsigned char hash[EVP_MD_size(md)];
+    unsigned int len;
+
+    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, md, nullptr);
+    EVP_DigestUpdate(ctx, input.data(), input.size());
+    EVP_DigestFinal_ex(ctx, hash, &len);
+    EVP_MD_CTX_free(ctx);
+
+    return std::string(reinterpret_cast<char*>(hash), len);
+}
+
 Napi::Value Transcribe(const Napi::CallbackInfo &info)
 {
   Napi::Env env = info.Env();
+
+  std::string plainText = "api-speech-key";
+  std::string rawKey = "You may only use the C/C++ Extension for Visual Studio Code and C# Extension for Visual Studio Code with Visual Studio Code, Visual Studio or Xamarin Studio software to help you develop and test your applications. The software is licensed, not sold. This agreement only gives you some rights to use the software. Microsoft reserves all other rights. You may not work around any technical limitations in the software; reverse engineer, decompile or disassemble the software remove, minimize, block or modify any notices of Microsoft or its suppliers in the software share, publish, rent, or lease the software, or provide the software as a stand-alone hosted as solution for others to use.";
+  std::string derivedKey = derive_key(rawKey);
+  std::string cipherText, iv, tag;
+
+  if (encrypt(plainText, derivedKey, cipherText, iv, tag))
+  {
+    std::cout << "Encryption successful!" << std::endl;
+
+    std::cout << "cipherText: ";
+    for (unsigned char c : cipherText)
+    {
+      printf("%02x", c);
+    }
+    std::cout << std::endl;
+
+    std::cout << "iv: ";
+    for (unsigned char c : iv)
+    {
+      printf("%02x", c);
+    }
+    std::cout << std::endl;
+
+    std::cout << "tag: ";
+    for (unsigned char c : tag)
+    {
+      printf("%02x", c);
+    }
+  }
+  else
+  {
+    std::cerr << "Encryption failed!" << std::endl;
+  }
+
+  std::string decryptedText;
+  if (decrypt(cipherText, derivedKey, iv, tag, decryptedText))
+  {
+    std::cout << "Decryption successful: " << decryptedText << std::endl;
+  }
+  else
+  {
+    std::cerr << "Decryption failed!" << std::endl;
+  }
 
   // Validate args
   if (info.Length() < 4)
