90c3bc2f9d118fe5f789748507c586ab216805ee
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2024-07-08 09:57:23 -0700

    more fixes

--- setup-packages.ps1
+++ setup-packages.ps1
@@ -1,6 +1,8 @@
 $SDK_VERSION = "1.37.0"
 $TEMP_PACKAGE_DIR = "temp_packages"
 
+mkdir $TEMP_PACKAGE_DIR
+
 dotnet add package Microsoft.CognitiveServices.Speech --version $SDK_VERSION --package-directory $TEMP_PACKAGE_DIR
 dotnet add package Microsoft.CognitiveServices.Speech.Extension.Embedded.SR --version $SDK_VERSION --package-directory $TEMP_PACKAGE_DIR
 dotnet add package Microsoft.CognitiveServices.Speech.Extension.Embedded.TTS --version $SDK_VERSION --package-directory $TEMP_PACKAGE_DIR
